de77044f6e93fbdb54a74859534448e6c2abc556
Rafael <rcheruti@gmail.com>
2017-03-16 09:29:21 -0300

    Add menu

--- src/js/normal/index.js
+++ src/js/normal/index.js
@@ -1,5 +1,44 @@
 $(function(){
   
+  //===========   Menu   =================
+  var menuPanel = $('.menuPanel');
+  $('.menuBtn').on('click touchstart', function(ev){
+    ev.preventDefault();
+    ev.stopPropagation();
+    
+    if(menuPanel.is('.active')){
+      menuPanel.removeClass('active');
+    }else{
+      menuPanel.addClass('active');
+    }
+  });
+  menuPanel.on('click touchstart', function(ev){
+    ev.preventDefault();
+    ev.stopPropagation();
+  });
+  $(document).on('click touchstart', function(ev){
+    ev.preventDefault();
+    menuPanel.removeClass('active');
+  });
+  
+  
+  $('[s-to]').on('click touchstart', function(ev){
+    ev.preventDefault();
+    
+    var sel = $(this).attr('s-to');
+    var el = $(sel);
+    if(el.length){
+      document.body.scrollTop = el[0].offsetTop - 20 ;
+    }
+  });
+  
+  
+  
+  
+  //===========   Examples   =================
+  
+  
+  
   
   
 });
